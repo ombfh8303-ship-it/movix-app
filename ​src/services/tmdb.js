ca6f03c55b4dd3b91a6862c98c1d1f63bfa89a0c
ombfh8303-ship-it <ombfh8303@gmail.com>
2026-09-08 14:58:19 +0300

    Update tmdb.js

--- ​src/services/tmdb.js
+++ ​src/services/tmdb.js
@@ -1,1 +1,14 @@
 
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+export const getTrendingMovies = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results || [];
+  } catch (error) {
+    console.error("Error fetching movies:", error);
+    return [];
+  }
+};
