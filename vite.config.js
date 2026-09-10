fa1ed5829a2d35afface9ed9dfc4677bee9818bb
ombfh8303-ship-it <ombfh8303@gmail.com>
2026-09-10 13:38:57 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ship-it/', // أو اسم مستودعك الخاص إذا تغير
+  base: '/movix-app/',
 })
