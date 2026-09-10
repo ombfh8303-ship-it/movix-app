36a9a05379c42a7fcc5056c7d82b89213445406b
ombfh8303-ship-it <ombfh8303@gmail.com>
2026-09-10 12:23:40 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: './index.html'
+    }
   }
 })
